f6299acc70e626a0c29bf992e946a1d53bff0fdd
kevin stollaerd <kevin.stollaerd@gmail.com>
2016-10-25 20:41:18 +0200

    Keypad

---
+++ Kamer/Assets/Scripts/Keypad/InstantiateKeypad.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class InstantiateKeypad : MonoBehaviour {
+
+    public Transform go, parent;
+    public Keypad kp;
+
+    void Awake()
+    {
+        kp = new Keypad(go, parent);
+    }
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
